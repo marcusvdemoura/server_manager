f8f13fba27a4dfbe25ce1300c0ed29a59076bbf3
marcusvdemoura <marcusdemoura@outlook.com>
2022-06-03 14:46:33 +0100

    Configuring remaining calls

--- src/main/java/com/marcusmoura/server/ServerApplication.java
+++ src/main/java/com/marcusmoura/server/ServerApplication.java
@@ -1,13 +1,40 @@
 package com.marcusmoura.server;
 
+import com.marcusmoura.server.enumeration.Status;
+import com.marcusmoura.server.model.Server;
+import com.marcusmoura.server.repositories.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository){
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux",
+					"16GB", "Personal PC",
+					"http://localhost:8080/server/image/server1.png",
+					Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.58", "Fedora Linux",
+					"16GB", "Dell Tower",
+					"http://localhost:8080/server/image/server2.png",
+					Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.21", "MS 2008",
+					"32GB", "Web Server",
+					"http://localhost:8080/server/image/server3.png",
+					Status.SERVER_DOWN));
+			serverRepository.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux",
+					"64GB", "Mail Server",
+					"http://localhost:8080/server/image/server4.png",
+					Status.SERVER_DOWN));
+		};
 	}
 
 }
--- src/main/java/com/marcusmoura/server/model/Server.java
+++ src/main/java/com/marcusmoura/server/model/Server.java
@@ -19,11 +19,13 @@ public class Server {
     private Long id;
     @Column(unique = true)
     @NotEmpty(message = "Ip address cannot be empty or null")
-    private String ipAdress;
+    private String ipAddress;
     private String name;
     private String memory;
     private String type;
     private String imageUrl;
     private Status status;
+
+
 
 }
